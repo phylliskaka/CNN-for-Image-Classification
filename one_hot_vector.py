cbc5b3be5372218c16b433a4dc96b119ee8654fb
Wenxiao Zhan <19960229zhan@gmail.com>
2018-11-02 18:08:24 -0400

    Add files via upload

---
+++ one_hot_vector.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  2 11:50:02 2018
+
+@author: phyllis
+"""
+
+import numpy as np 
+
+def convert_to_one_hot(label, n):
+    """
+    input: label is an array like object, shape(m, 1) 
+    n is an integer object, equal to the maximum value of label[0] -1 
+    output an matrix of dimension (m, n)
+    """
+    m = label.shape[0]
+
+    one_hot_matrix = np.ndarray((m, n))
+    one_hot_vector = np.zeros((n))
+    for i in range(m):
+        one_hot_vector[int(label[i][0]-1)] = 1
+        one_hot_matrix[i] = one_hot_vector
+        one_hot_vector = np.zeros((n))
+    
+    return one_hot_matrix
+
+a = np.ones((30, 1))
+x = convert_to_one_hot(a, 3)
+        
