47d6be8ef68c31b1e4a83dc52fb1f1cac38f36dd
Wenxiao Zhan <19960229zhan@gmail.com>
2019-02-28 11:07:36 -0600

    Add files via upload

---
+++ ResNet50_scratch/load_dataset.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: phyllis
+"""
+
+import numpy as np 
+import os 
+import cv2
+
+#skitlearn 
+from sklearn.cross_validation import train_test_split 
+
+#%%
+
+def load_dataset():
+    """
+    output: X_train, X_test, Y_train, Y_test 
+    """
+    def get_img(data_path):
+        """
+        read the image and transfer it to an array like object
+        """
+        img = cv2. imread(data_path)
+        img = cv2.resize(img, (64, 64))
+        return img
+    
+    path1 = '/home/phyllis/Desktop/ResNet50/input_dataset' #dataset path 
+    
+    listing = os.listdir(path1)
+    m = len(listing)
+    
+    X = np.ndarray((m, 64, 64, 3)) # store the image matrix 
+    Y = np.ndarray((m, 1)) #store the image label 
+    
+    for i in range(m):
+        im = get_img(path1 + '/' + listing[i])
+        X[i] = im
+        Y[i] = (int(listing[i][0])) #file name is "label_index" like, retrieve the label from file name
+    classes = int(max(Y)[0] + 1) # store the image classes number, in this project classes =10
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size= 0.2, random_state = 0)
+        
+    return X_train, X_test, Y_train, Y_test, classes 
+    
+
+
+
+    
